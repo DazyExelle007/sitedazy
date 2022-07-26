5b89cd3fa0ef6adf23e7ec5a12e68630a41968e7
DazyExelle007 <luciarasambahita1999gmail.com>
2022-07-26 19:02:05 +0300

    charge my siteweb blog

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Social from '../components/Social';
+import Monprofil from "../assets/img/maki.jpg"
+import Myboutton from '../components/Myboutton';
+
 
 const Home = () => {
     return (
         <div>
             <Navigation/>
             <div className='homepages'>
-                <div className='left-homepages'>
-                    Je suis devellpeur autodidacte
-                </div>
-                <div className='right-homepages'>
-                    <image src=""></image>
-                </div>
+                   <div className=' container  container-flex'>
+                    <div className='content-left'>
+                    <h1> Salama, Je m'appel Dazy Exelle </h1>
+                    
+                    <p>Je suis un concepteur Web Desingner et Developer</p>
+                    </div>
+                   
+                   <div className='content-right'>
+                     <img src={Monprofil} alt="" />
+                   </div>
+                   <Myboutton/>
+                   </div>
+                   
+                 
+                   
             </div>
+            <Social/>
         </div>
     );
 };
