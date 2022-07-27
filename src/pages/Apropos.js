eb87da9138919195e8de04f05d9cd44140d83c98
DazyExelle007 <luciarasambahita1999gmail.com>
2022-07-27 15:09:33 +0300

    Add contente about and add style

--- src/pages/Apropos.js
+++ src/pages/Apropos.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Monprofil from "../assets/img/maki.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+
 
 const Apropos = () => {
     return (
         <div>
             <Navigation/>
-            <h1>A-propos</h1>
+           <div className='group-about'>
+            <div className='left-about'>
+                <div className="about-title">
+                <h1>Tous a savoir sur moi</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio dolores accusantium ipsam quia nostrum.
+                     Aspernatur voluptatibus quod consectetur sit consequuntur odio voluptatem, 
+                     obcaecati tempora rem natus eos sint saepe doloribus?</p>
+                </div>
 
-            <br/>
+                <ul className='all-info'>
+                    <li>
+                     <h1>Pastemps</h1>
+                    <div className='content'>
+                        <p>Lire</p>
+                        <p>Music</p>
+                    </div>
+                    </li>
+                    <li>
+                     <h1>Pastemps</h1>
+                    <div className='content'>
+                        <p>Lire</p>
+                        <p>Music</p>
+                    </div>
+                    </li>
+                    <li>
+                     <h1>Pastemps</h1>
+                    <div className='content'>
+                        <p>Lire</p>
+                        <p>Music</p>
+                    </div>
+                    </li>
+                </ul>
+                
+            </div>
 
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt delectus voluptatibus quos, hic,
-                 libero iste quam tempore nulla architecto non, quas vel beatae commodi deserunt odit nisi mollitia quisquam sint?</p>
+            <div className='right-about'>
+                <img src={Monprofil} />
+            </div>
+           </div>
         </div>
     );
 };
