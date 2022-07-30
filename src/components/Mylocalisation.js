ba45b904889f80d150101b89e609caee65ccd66e
DazyExelle007 <luciarasambahita1999gmail.com>
2022-07-30 15:07:26 +0300

    add map localisation

--- src/components/Mylocalisation.js
+++ src/components/Mylocalisation.js
@@ -1,27 +1,12 @@
-import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React from 'react';
 
+const Mylocalisation = () => {
+    return (
+        
+            <div className=''><iframe src="https://maps.google.com/maps?q=Fianarantsoa&t=&z=11&ie=UTF8&iwloc=&output=embed" title='mymap' ></iframe>
+           </div>
+        
+    );
+};
 
-
-
-class Mylocalisation extends Component {
-
-    render() {   
-        return (
-            <Map google={this.props.google} zoom={14}>
-            <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
-     
-            <InfoWindow onClose={this.onInfoWindowClose}>
-                <div>
-                   <h1></h1>
-                </div>
-            </InfoWindow>
-          </Map>
-        );
-    }
-}
-
-export default GoogleApiWrapper({
-    apiKey: ("AIzaSyCBbqS5bEAPKVwNUF6VHRhxoKWCLf6WSnQ")
-  })(Mylocalisation)
+export default Mylocalisation;
